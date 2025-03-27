f298370d01b6b41be9d02c05e4e1dea29b1c45fd
LiuLei-jm <14699396@qq.com>
2025-03-27 16:01:33 +0800

    Global Error Handling

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,5 +1,8 @@
+using BuberDinner.Api.Errors;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,12 +10,22 @@ var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddApplication().AddInfrastructure(builder.Configuration);
     builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 
 
 {
+    app.UseExceptionHandler("/error");
+    //app.Map(
+    //    "/error",
+    //    (HttpContext httpContext) =>
+    //    {
+    //        Exception? exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+    //        return Results.Problem();
+    //    }
+    //);
     app.UseHttpsRedirection();
 
     app.MapControllers();
